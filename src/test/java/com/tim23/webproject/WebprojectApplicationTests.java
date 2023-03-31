bb3e5a9350fe0c20d2ecac01251342d252b23d2c
Tijana Petrovic <tijanapetrovicns03@gmail.com>
2023-03-31 10:05:01 +0200

    inicijalni projekat

---
+++ src/test/java/com/tim23/webproject/WebprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim23.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
